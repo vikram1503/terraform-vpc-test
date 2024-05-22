974eeeebf27f8d12b23b357e3cc1b5f713a37e39
vikram1503 <vikram15031989@gmail.com>
2024-05-22 20:05:06 +0530

    practice

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/vikram1503/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
