ccdd3ae52b6520fa9be4d50dbb9c630131ada76f
vikram1503 <vikram15031989@gmail.com>
2024-05-22 17:46:11 +0530

    practice

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+# output "azs_info" {
+#     value = module.vpc.azs
+  
+# }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
---
+++ vpc.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+    project_name = var.project_name
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    db_subnet_cidrs = var.db_subnet_cidrs
+    is_peering_required = var.is_peering_required
+}
